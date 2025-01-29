9ebdede42f390bfefede02504200b3ea3fc2ac9d
Nishwa <nishwashaikh135@gmail.com>
2025-01-29 23:24:03 +0500

    Removed NaN values from RunTime

--- Data_cleaning_1.py
+++ Data_cleaning_1.py
@@ -1,3 +1,7 @@
 import pandas as pd
 PD=pd.read_csv('C:/Users/PMLS/OneDrive/Desktop/Data cleaning/archive.zip')
-print(PD)
+print('Raw data:')
+print(PD.head(20))
+print('refined data:')
+PD['RunTime']=PD['RunTime'].ffill()
+print(PD.head(20))
